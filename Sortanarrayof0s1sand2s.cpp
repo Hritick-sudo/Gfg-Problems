d245ce8bc75514768656bfc7819df107af837698
Hritick-sudo <66056834+Hritick-sudo@users.noreply.github.com>
2022-10-07 11:51:52 +0530

    Create Sortanarrayof0s1sand2s.cpp

---
+++ Sortanarrayof0s1sand2s.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        int i = 0;
+        int j = 0;
+        int k = n-1;
+        while(j<=k)
+        {
+            if(a[j] == 0)
+            {
+                swap(a[i],a[j]);
+                i++;
+                j++;
+            }
+            else if(a[j] == 1)
+            {
+                j++;
+            }
+            else if(a[j] == 2)
+            {
+                swap(a[j],a[k]);
+                k--;
+            }
+        }
+    }
+    
+};
